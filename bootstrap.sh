e36eaebafc6ecbfbede7e85e101d611e1fe96c6e
Punyapat Sessomboon <perth.s28@gmail.com>
2019-09-15 11:15:50 +0700

    try to fix path

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 
 MUNIN_CONFIGURATION_FILE=/etc/munin/munin-node.conf
 MUNIN_LOG_FILE=/var/log/munin/munin-node-configure.log
+MUNIN_LIBDIR=/usr/share/munin
+
+ln -s /usr/share/munin/plugins /plugins
 
 if [ ! -z "$ALLOW" ]; then
     echo $ALLOW >> $MUNIN_CONFIGURATION_FILE
